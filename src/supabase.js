8c35585fea53e57bb3786a757ac99a6816a88c1f
abasstobiloba91-svg <abasstobiloba91@gmail.com>
2026-03-13 16:08:42 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
-// pull in values from environment for deployment
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://eryeaaomkkzyyklnfaxa.supabase.co';
-export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyeWVhYW9ta2t6eXlrbG5mYXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTE0NDUsImV4cCI6MjA4ODM4NzQ0NX0.WxLQQuvWLcQLfMYJ4cqj3YM9GtHsMiRVt5Dt4IK1o5c';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+import { createClient } from "@supabase/supabase-js";
+const SUPA_URL = "https://flfjlaaajsdzbhldgvso.supabase.co";
+const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZmpsYWFhanNkemJobGRndnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NjQ1MzcsImV4cCI6MjA4ODQ0MDUzN30.ka2SSllYgDF4cgGkJfw4cMUac8z3OkpmZC4JF_umCIQ";
+export const supabase = createClient(SUPA_URL, SUPA_KEY);
