47ec5257d941602485187eee321cb72f02e6bc7c
abasstobiloba91-svg <abasstobiloba91@gmail.com>
2026-03-14 06:14:08 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPA_URL = "https://flfjlaaajsdzbhldgvso.supabase.co";
-const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZmpsYWFhanNkemJobGRndnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NjQ1MzcsImV4cCI6MjA4ODQ0MDUzN30.ka2SSllYgDF4cgGkJfw4cMUac8z3OkpmZC4JF_umCIQ";
+const SUPA_URL = "https://eryeaaomkkzyyklnfaxa.supabase.co";
+const SUPA_KEY = "sb_publishable_41dhWT4sA8ber_ZnS0A51A_24w6KUUA";
 const supabase = createClient(SUPA_URL, SUPA_KEY);
 
 
